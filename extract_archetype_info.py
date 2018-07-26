c9eb0303073e2145a3c9543291fd257424662430
Jerome Leclanche <jerome@leclan.ch>
2018-07-26 20:33:20 +0300

    Create an archetypes.po file and script to generate it

---
+++ extract_archetype_info.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+from datetime import datetime
+
+import requests
+from polib import POEntry, POFile
+
+
+def main():
+	po = POFile()
+	out_path = os.path.join(
+		os.path.dirname(__file__), "en", "LC_MESSAGES", "archetypes.po"
+	)
+
+	po.metadata = {
+		"Project-Id-Version": "hsreplaynet",
+		"Report-Msgid-Bugs-To": "",
+		"POT-Creation-Date": datetime.now().isoformat(),
+		"Last-Translator": "HearthSim <admin@hearthsim.net>",
+		"Language-Team": "English",
+		"MIME-Version": "1.0",
+		"Content-Type": "text/plain; charset=utf-8",
+		"Content-Transfer-Encoding": "8bit",
+	}
+
+	r = requests.get("https://api.hsreplay.net/v1/archetypes/")
+	for archetype in r.json():
+		name = archetype.get("name", "")
+		url = archetype.get("url", "")
+		if not name or not url:
+			continue
+
+		entry = POEntry(
+			msgid=name,
+			msgstr="",
+			occurences=[("https://hsreplay.net" + url, "34")]
+		)
+		po.append(entry)
+
+	po.save(out_path)
+	print(f"Written {out_path}")
+
+
+if __name__ == "__main__":
+	main()
